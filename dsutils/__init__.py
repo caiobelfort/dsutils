088eedaba5fb8999f2e0fd1594d5a6543edb856b
Caio Belfort <caiobelfort90@gmail.com>
2017-11-14 09:15:41 -0300

    init

---
+++ dsutils/__init__.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+
+from db_connection_manager import DBConnectionManager
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dsutils',
+    version='0.01a',
+    description='Utilidades para data science',
+    author='caiobelfort',
+    author_email='caiobelfort90@gmail.com',
+    license='GPL',
+    packages=['dsutils'],
+    zip_safe=False
+)
